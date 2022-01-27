ff417f23b786f5ce5bd41b52851caef2c1ea8987
Yamakks <maxim5_91@mail.ru>
2022-01-27 15:06:47 +0300

    1.1. Что такое import [#504773]

---
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -0,0 +1,14 @@
+package ru.job4j.math;
+
+@SuppressWarnings("checkstyle:EmptyLineSeparator")
+public class MathFunction {
+
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+
+}
