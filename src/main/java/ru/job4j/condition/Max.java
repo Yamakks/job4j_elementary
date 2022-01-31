9bbd940882dab522691f71a55b1eba808fa713f0
Yamakks <maxim5_91@mail.ru>
2022-01-31 12:01:12 +0300

    4.2. Тернарное сравнение [#189 #258146]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,12 +2,7 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        if (left == right) {
-            result = right;
-        }
-        return result;
-
+        return  left >= right ? left : right;
     }
 
 }
