7cc3420319ea141b3474b93b7ceb40e8c59a20af
Yamakks <maxim5_91@MAIL/RU>
2022-02-08 02:08:50 +0300

    5.2. Создать программу, вычисляющую факториал. [#193]

---
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    @SuppressWarnings("checkstyle:RightCurly")
+    public static int calc(int n) {
+        int result = 1;
+        if (n == 0) {
+            result = 1;
+        } else {
+        for (int index = 1; index <= n; index++) {
+            result = result * index;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactorialTest {
+
+    @Test
+    public void whenCalculateFactorialForFiveThenOneHundredTwenty() {
+        int n = 5;
+        int expected = 120;
+        int result = Factorial.calc(n);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenCalculateFactorialForZeroThenOne() {
+        int n = 0;
+        int expected = 1;
+        int result = Factorial.calc(n);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenCalculateFactorialForSixThenSevenHundredAndTwenty() {
+        int n = 6;
+        int expected = 720;
+        int result = Factorial.calc(n);
+        Assert.assertEquals(expected, result);
+    }
+}
